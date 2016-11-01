54e10b34c6c9e80a95bba1cc82f8c8d93171d481
Juan Pablo <Juan Pablo@POP486-DELL>
2016-11-01 15:30:05 -0600

    Add project files.

---
+++ WebAPI/Controllers/PersonsController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    public class PersonsController : ApiController
+    {
+        List<Person> Persons = new List<Person>()
+        {
+           new Person() { id = 1, FirstName = "Juan", LastName = "León" },
+           new Person() { id = 2, FirstName = "Jose", LastName = "Portillo" },
+           new Person() { id = 3, FirstName = "Jacqueline", LastName = "Perez" }
+        };
+
+        public IEnumerable<Person> GetAllPersons()
+        {
+            return Persons;
+        }
+
+        public IHttpActionResult GetPerson(int id)
+        {
+            var person = Persons.FirstOrDefault((p) => p.id == id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+            return Ok(person);
+        }
+
+        [HttpPost]
+        public IEnumerable<Person> GetAllPersons([FromBody] Person item)
+        {
+            Persons.Add(item);
+
+            return Persons;
+        }
+
+        [HttpPut]
+        public IEnumerable<Person> UpdatePerson([FromBody] Person item)
+        {
+            Persons[item.id].FirstName = item.FirstName;
+            Persons[item.id].LastName = item.LastName;
+
+            return Persons;
+        }
+
+        [HttpDelete]
+        public IHttpActionResult DelPerson([FromBody] Person item)
+        {
+            Persons.RemoveAt(item.id);
+
+            return Content(HttpStatusCode.NoContent, "");
+        }
+    }
+}
